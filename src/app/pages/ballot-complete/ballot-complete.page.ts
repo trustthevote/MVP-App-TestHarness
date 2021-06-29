3211d2d83fe4e0f2a281e5d3400fd6e5be7ed679
Noel <nfranceschijr@gmail.com>
2021-06-29 07:38:09 -0400

    added logic for switching precinct

--- src/app/pages/ballot-complete/ballot-complete.page.ts
+++ src/app/pages/ballot-complete/ballot-complete.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Browser } from '@capacitor/browser';
 
@@ -7,10 +7,21 @@ import { Browser } from '@capacitor/browser';
   templateUrl: './ballot-complete.page.html',
   styleUrls: ['./ballot-complete.page.scss'],
 })
-export class BallotCompletePage {
+export class BallotCompletePage implements OnInit {
   paramData: any;
-  // localPdf = 'assets/files/bedrock_precinct2/jetsons_bedrock-precinct2_ballot_marked.pdf';
-  // localPdf2 = 'assets/files/bedrock_precinct2/precinct2_voter_affidavit.pdf';
+  precinctNum: number;
+
+  // P1 ballot - https://drive.google.com/file/d/1NjMf2sUwjTPH20jN4ajnWRZDvkobq9hR/view?usp=sharing
+  // P1 aff - https://drive.google.com/file/d/1A8v82etRnePtdF_IZDQmVb34Ss2tZu-f/view?usp=sharing
+
+  // P2 ballot - https://drive.google.com/file/d/1FtiAIYsOdGoLSqXBEymGatkpR7heuSfI/view?usp=sharing
+  // P2 aff - https://drive.google.com/file/d/1Qbc4jLYFkDoljpR4g5NFJ4Wfc_S5--kc/view?usp=sharing
+
+  // P3 ballot - https://drive.google.com/file/d/1hKmtHmpJ2qP1atsDWfU2qtzDOn6kZ1ra/view?usp=sharing
+  // P3 aff - https://drive.google.com/file/d/1j3wzbIfH8Sm4UAyij6N5pjHJVXTGWliJ/view?usp=sharing
+
+  // P4 ballot - https://drive.google.com/file/d/1KyESAe8iVuA61zEKhpN3DENtNOE6M8Lr/view?usp=sharing
+  // P4 aff - https://drive.google.com/file/d/1n3iNq1KVvr8YjKllMO6znArKtlDvvK-k/view?usp=sharing
 
   constructor(private router: Router) {
     if (this.router.getCurrentNavigation().extras.state) {
@@ -18,19 +29,45 @@ export class BallotCompletePage {
     }
   }
 
+  ngOnInit() {
+    const lastName = this.paramData.lastname.charAt(0).toUpperCase() + this.paramData.lastname.slice(1);
+    if (lastName.includes('A', 0)) {
+      this.precinctNum = 1;
+    } else if(lastName.includes('B', 0)) {
+      this.precinctNum = 2;
+    } else if (lastName.includes('C', 0)) {
+      this.precinctNum = 3;
+    } else {
+      this.precinctNum = 4;
+    }
+  }
+
+
   goToFinish() {
     this.router.navigate(['print']);
   }
 
-  async openPDF() {
-    await Browser.open({ url: 'https://drive.google.com/file/d/1KyESAe8iVuA61zEKhpN3DENtNOE6M8Lr/view?usp=sharing' });
+  async openPDF(precinctNum) {
+    if (precinctNum === 1) {
+      await Browser.open({ url: 'https://drive.google.com/file/d/1NjMf2sUwjTPH20jN4ajnWRZDvkobq9hR/view?usp=sharing' });
+    } else if(precinctNum === 2) {
+      await Browser.open({ url: 'https://drive.google.com/file/d/1FtiAIYsOdGoLSqXBEymGatkpR7heuSfI/view?usp=sharing' });
+    } else if (precinctNum === 3) {
+      await Browser.open({ url: 'https://drive.google.com/file/d/1hKmtHmpJ2qP1atsDWfU2qtzDOn6kZ1ra/view?usp=sharing' });
+    } else {
+      await Browser.open({ url: 'https://drive.google.com/file/d/1KyESAe8iVuA61zEKhpN3DENtNOE6M8Lr/view?usp=sharing' });
+    }
   }
 
-  async openPDF2() {
-    await Browser.open({ url: 'https://drive.google.com/file/d/1Qbc4jLYFkDoljpR4g5NFJ4Wfc_S5--kc/view?usp=sharing' });
+  async openPDF2(precinctNum) {
+    if (precinctNum === 1) {
+      await Browser.open({ url: 'https://drive.google.com/file/d/1A8v82etRnePtdF_IZDQmVb34Ss2tZu-f/view?usp=sharing' });
+    } else if(precinctNum === 2) {
+      await Browser.open({ url: 'https://drive.google.com/file/d/1Qbc4jLYFkDoljpR4g5NFJ4Wfc_S5--kc/view?usp=sharing' });
+    } else if (precinctNum === 3) {
+      await Browser.open({ url: 'https://drive.google.com/file/d/1j3wzbIfH8Sm4UAyij6N5pjHJVXTGWliJ/view?usp=sharing' });
+    } else {
+      await Browser.open({ url: 'https://drive.google.com/file/d/1n3iNq1KVvr8YjKllMO6znArKtlDvvK-k/view?usp=sharing' });
+    }
   }
-
-  // async goToPrint() {
-  //   await this.router.navigate(['print']);
-  // }
 }
--- src/app/pages/ballot-form/ballot-form.page.ts
+++ src/app/pages/ballot-form/ballot-form.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
@@ -6,12 +6,26 @@ import { NavigationExtras, Router } from '@angular/router';
   templateUrl: './ballot-form.page.html',
   styleUrls: ['./ballot-form.page.scss'],
 })
-export class BallotFormPage {
+export class BallotFormPage implements OnInit {
   paramData: any;
+  precinct: any;
 
   constructor(private router: Router) {
     if (this.router.getCurrentNavigation().extras.state) {
       this.paramData = this.router.getCurrentNavigation().extras.state.user;
+    }
+  }
+
+  ngOnInit() {
+    const lastName = this.paramData.lastname.charAt(0).toUpperCase() + this.paramData.lastname.slice(1);
+    if (lastName.includes('A', 0)) {
+      this.precinct = 'Your Ballot is for Precinct #1';
+    } else if(lastName.includes('B', 0)) {
+      this.precinct = 'Your Ballot is for Precinct #2';
+    } else if (lastName.includes('C', 0)) {
+      this.precinct = 'Your Ballot is for Precinct #3';
+    } else {
+      this.precinct = 'Your Ballot is for Precinct #4';
     }
   }
 
@@ -25,6 +39,4 @@ export class BallotFormPage {
   goBack() {
     this.router.navigate(['ballot']);
   }
-
-
 }
