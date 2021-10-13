3e855b10eeb0e8e5e5de6653ad7a355191d9cab1
Matt Beattie <mattgbeattie@gmail.com>
2021-10-12 21:01:42 -0400

    voterartifact contents are now fetched from assets, pdfs are base64 encoded

--- src/app/api/voterartifacts.service.ts
+++ src/app/api/voterartifacts.service.ts
@@ -4,62 +4,86 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class VoterartifactsService {
-precinctId: string;
-precinct: string;
-cvrfile: string;
-affidavitfile: string;
-ballotfile: string;
-constructor() {}
+  precinctId: string;
+  precinct: string;
+  cvrfile: string;
+  affidavitfile: string;
+  ballotfile: string;
 
-Initialize(lastName: string) {
-  let key = lastName.substring(0, 1).toUpperCase();
-  switch (key) {
-    case 'A':
-      this.precinctId = "precinct_1";
-      this.precinct = "Precinct 1 Spaceport";
-      this.cvrfile = "src/assets/files/port_precinct1/jetsons_port_precinct1_cvr.xml";
-      this.affidavitfile = "src/assets/files/port_precinct1/precinct1_voter_affidavit.pdf";
-      this.ballotfile = "src/assets/files/port_precinct1/jetsons_port-precinct1_ballot_marked.pdf";
-      break;
-    case 'B':
-      this.precinctId = "precinct_2";
-      this.precinct = "Precinct 2 Bedrock";
-      this.cvrfile = "src/assets/files/bedrock_precinct2/jetsons_bedrock-precinct2_cvr.xml";
-      this.affidavitfile = "src/assets/files/bedrock_precinct2/precinct2_voter_affidavit.pdf";
-      this.ballotfile = "src/assets/files/bedrock_precinct2/jetsons_bedrock-precinct2_ballot_marked.pdf";
-      break;
-    case 'C':
-      this.precinctId = "precinct_3";
-      this.precinct = "Precinct 3 Downtown";
-      this.cvrfile = "src/assets/files/downtown_precinct3/jetsons_downtown-precinct3_cvr.xml";
-      this.affidavitfile = "src/assets/files/downtown_precinct3/precinct3_voter_affidavit.pdf";
-      this.ballotfile = "src/assets/files/downtown_precinct3/jetsons_downtown-precinct3_ballot_marked.pdf";
-      break;
-    default:
-      this.precinctId = "precinct_4";
-      this.precinct = "Precinct 4 Spacetown";
-      this.cvrfile = "src/assets/files/spacetown_precinct4/jetsons_spacetown_precinct4_cvr.xml";
-      this.affidavitfile = "src/assets/files/spacetown_precinct4/jetsons_spacetown-precinct4_ballot_marked.pdf";
-      this.ballotfile = "src/assets/files/spacetown_precinct4/precinct4_voter_affidavit.pdf";
-      break
+  constructor() {}
 
-  };
-  return this.precinct;
-}
+  Initialize(lastName: string) {
+    let key = lastName.substring(0, 1).toUpperCase();
+    switch (key) {
+      case 'A':
+        this.precinctId = "precinct_1";
+        this.precinct = "Precinct 1 Spaceport";
+        fetch('./assets/files/port_precinct1/jetsons_downtown-precinct1_cvr.xml')
+          .then(res => res.text())
+          .then(file => this.cvrfile = file);
+        fetch('./assets/files/port_precinct1/precinct1_voter_affidavit.pdf')
+          .then(res => res.text())
+          .then(file => this.affidavitfile = btoa(unescape(encodeURIComponent(file))));
+        fetch('./assets/files/port_precinct1/jetsons_port-precinct1_ballot_marked.pdf')
+          .then(res => res.text())
+          .then(file => this.ballotfile = btoa(unescape(encodeURIComponent(file))));
+        break;
+      case 'B':
+        this.precinctId = "precinct_2";
+        this.precinct = "Precinct 2 Bedrock";
+        fetch('./assets/files/bedrock_precinct2/jetsons_bedrock-precinct2_cvr.xml')
+          .then(res => res.text())
+          .then(file => this.cvrfile = file);
+        fetch('./assets/files/bedrock_precinct2/precinct2_voter_affidavit.pdf')
+          .then(res => res.text())
+          .then(file => this.affidavitfile = btoa(unescape(encodeURIComponent(file))));
+        fetch('./assets/files/bedrock_precinct2/jetsons_bedrock-precinct2_ballot_marked.pdf')
+          .then(res => res.text())
+          .then(file => this.ballotfile = btoa(unescape(encodeURIComponent(file))));
+        break;
+      case 'C':
+        this.precinctId = "precinct_3";
+        this.precinct = "Precinct 3 Downtown";
+        fetch('./assets/files/downtown_precinct3/jetsons_downtown-precinct3_cvr.xml')
+          .then(res => res.text())
+          .then(file => this.cvrfile = file);
+        fetch('./assets/files/downtown_precinct3/precinct3_voter_affidavit.pdf')
+          .then(res => res.text())
+          .then(file => this.affidavitfile = btoa(unescape(encodeURIComponent(file))));
+        fetch('./assets/files/downtown_precinct3/jetsons_downtown-precinct3_ballot_marked.pdf')
+          .then(res => res.text())
+          .then(file => this.ballotfile = btoa(unescape(encodeURIComponent(file))));
+        break;
+      default:
+        this.precinctId = "precinct_4";
+        this.precinct = "Precinct 4 Spacetown";
+        fetch('./assets/files/spacetown_precinct4/jetsons_spacetown_precinct4_cvr.xml')
+          .then(res => res.text())
+          .then(file => this.cvrfile = file);
+        fetch('./assets/files/spacetown_precinct4/jetsons_spacetown-precinct4_ballot_marked.pdf')
+          .then(res => res.text())
+          .then(file => this.affidavitfile = btoa(unescape(encodeURIComponent(file))));
+        fetch('./assets/files/spacetown_precinct4/precinct4_voter_affidavit.pdf')
+          .then(res => res.text())
+          .then(file => this.ballotfile = btoa(unescape(encodeURIComponent(file))));
+        break
+    };
+    return this.precinct;
+  }
 
-public get precinctName(): string {
-  return this.precinct;
-}
+  public get precinctName(): string {
+    return this.precinct;
+  }
 
-public get cvr(): string {
-  return this.cvrfile; 
-}
+  public get cvr(): string {
+    return this.cvrfile;
+  }
 
-public get affidavit(): string {
-  return this.affidavitfile; 
-}
+  public get affidavit(): string {
+    return this.affidavitfile; 
+  }
 
-public get ballot(): string {
-  return this.ballotfile; 
-}
+  public get ballot(): string {
+    return this.ballotfile; 
+  }
 }
