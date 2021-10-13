23f4282ec480e49bacb62d0b8e06aa584bff6476
Matt Beattie <mattgbeattie@gmail.com>
2021-10-13 18:01:01 -0400

    fixes tests

--- src/app/api/voterartifacts.service.spec.ts
+++ src/app/api/voterartifacts.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { VoterartifactsService } from './voterartifacts.service';
 
-fdescribe('VoterartifactsService', () => {
+describe('VoterartifactsService', () => {
   let service: VoterartifactsService;
 
   beforeEach(() => {
