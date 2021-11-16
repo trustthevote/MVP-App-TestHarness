c5d46127c9e298c65cd4d453eeb14bb657fe61fa
Matt Beattie <mattgbeattie@gmail.com>
2021-11-15 20:35:03 -0500

    increases coverage

--- src/app/class/local-storage-ref/local-storage-ref.stub.ts
+++ src/app/class/local-storage-ref/local-storage-ref.stub.ts
@@ -1,7 +1,7 @@
 export class LocalStorageRefStub {
   store = {};
   mockLocalStorage = {
-    getItem: (key: string): string => key in this.store ? this.store[key] : null,
+    getItem: (key: string): string => (key in this.store ? this.store[key] : null),
     setItem: (key: string, value: string) => {
       this.store[key] = value;
     },
