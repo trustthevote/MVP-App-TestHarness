a68f664f61006353f50cab827b1ef08fa4cfb6a8
Matt Beattie <mattgbeattie@gmail.com>
2021-12-20 23:40:43 -0500

    tweak

--- src/app/api/avclient.service.ts
+++ src/app/api/avclient.service.ts
@@ -42,7 +42,7 @@ export class AvclientService {
   async requestAccessCode(opaqueVoterId: string): Promise<void> {
     await this.client.initialize();
 
-    const email = `mvptuser@yahoo.com`;
+    const email = 'mvptuser@yahoo.com';
     await this.client.requestAccessCode(opaqueVoterId, email);
   }
 
