74588a7879d00e62755f636c9559bd92c03500e4
awoflaherty <annewoflaherty@gmail.com>
2022-02-01 17:44:02 -0500

    Revert "Thaj 118 - Ballot Check Results UI changes Part 1 (#35)" (#36)
    This reverts commit ffe879ec54f1548b1cf3c13f1db06a2c1ae0ff52.

--- src/app/pages/test-results/test-results.page.ts
+++ src/app/pages/test-results/test-results.page.ts
@@ -54,4 +54,13 @@ export class TestResultsPage implements OnInit {
   failbtn() {
     this.route.navigate(['/ballot-test-failed-test']);
   }
+
+  backbtn() {
+    this.route.navigate([
+      '/ballot-fingerprint',
+      {
+        code: this.getCode,
+      },
+    ]);
+  }
 }
